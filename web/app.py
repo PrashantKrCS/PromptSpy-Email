fa6ddfbd48af0bc5095012f75c24e9940fd9112d
PrashantKrCS <35539577+PrashantKrCS@users.noreply.github.com>
2026-07-29 13:38:57 +0530

    Update app.py

--- web/app.py
+++ web/app.py
@@ -1,7 +1,4 @@
-from flask import Flask
-from flask import render_template
-
-from web.api import api
+from flask import Flask, render_template
 
 
 def create_app():
@@ -12,13 +9,12 @@ def create_app():
         static_folder="static"
     )
 
+    from web.api import api
+
     app.register_blueprint(api)
 
     @app.route("/")
     def index():
-
-        return render_template(
-            "index.html"
-        )
+        return render_template("index.html")
 
     return app
